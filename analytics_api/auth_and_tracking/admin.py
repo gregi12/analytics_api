948d1d4b1be6b287fabd88ea155cf314ac6d1e10
gregi12 <mlodypiki@o2.pl>
2023-12-25 19:49:45 +0100

    deleteing sensitve data
    deleteing sensitve data

--- analytics_api/auth_and_tracking/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import EmailUser, SiteInfo
-
-admin.site.register(EmailUser)
-admin.site.register(SiteInfo)
--- analytics_api/auth_and_tracking/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthAndTrackingConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "auth_and_tracking"
